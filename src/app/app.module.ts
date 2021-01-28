015de2f138935fa7b4e8aa4283bf02f438865601
coelhodiana <coelhodiana04@gmail.com>
2021-01-27 23:53:49 -0300

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { CartService } from './cart.service';
 
 @NgModule({
   imports: [
@@ -17,7 +18,7 @@ import { CartComponent } from './cart/cart.component';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: '/cart', component: CartComponent }
+      { path: 'cart', component: CartComponent }
     ])
   ],
   declarations: [
@@ -30,7 +31,8 @@ import { CartComponent } from './cart/cart.component';
   ],
   bootstrap: [
     AppComponent
-  ]
+  ],
+  providers: [CartService]
 })
 export class AppModule { }
 
