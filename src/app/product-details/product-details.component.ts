9cd62c1445ec81c1e5a7fe2fc4af8f8a1766ed0b
coelhodiana <coelhodiana04@gmail.com>
2021-01-28 00:21:41 -0300

    3 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from "@angular/router";
 
 import { products } from "../products";
 
+import { CartService } from "../cart.service";
+
 @Component({
   selector: "app-product-details",
   templateUrl: "./product-details.component.html",
@@ -12,7 +14,8 @@ export class ProductDetailsComponent implements OnInit {
   product;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService: CartService
   ) {}
 
   ngOnInit() {
@@ -22,5 +25,10 @@ export class ProductDetailsComponent implements OnInit {
 
     //Find the product that correspond with the id provided in route.
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
   }
 }
